e930a4fded767c42b3c5472d78b1f857c8c985c6
Jerry Li <jerryliuoft@gmail.com>
2020-08-23 16:35:17 -0400

    switched using webaudiofont replaced react-midi-sounds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import MIDISounds from "midi-sounds-react";
 import MIDIFile from "./MIDIFile";
+import WebAudioFontPlayer from "webaudiofont";
 
 const App = () => {
   const [currentMidi, setCurrentMidi] = useState(null);
   const fileSelectRef = useRef(null); // This is used to reset the file after uploading
-  const midiSoundRef = useRef(null); // This is the component that make the sound
+
+  const AudioContextFunc = window.AudioContext || window.webkitAudioContext;
+  const audioContext = new AudioContextFunc();
+  const player = new WebAudioFontPlayer();
+  const reverberator = player.createReverberator(audioContext);
+  reverberator.output.connect(audioContext.destination);
+  const input = reverberator.input;
 
   const selectFile = async (e) => {
     e.preventDefault();
@@ -21,35 +27,44 @@ const App = () => {
 
   // Loads the instruments and reset the uploader
   useEffect(() => {
-    if (!midiSoundRef || currentMidi === null || !fileSelectRef) {
+    if (currentMidi === null || !fileSelectRef) {
       return;
     }
     const song = currentMidi;
-    const player = midiSoundRef.current;
-
     for (let i = 0; i < song.tracks.length; i++) {
-      player.cacheInstrument(song.tracks[i].program);
+      let nn = player.loader.findInstrument(song.tracks[i].program);
+      let info = player.loader.instrumentInfo(nn);
+      console.log({ nn, info });
+      song.tracks[i].info = info;
+      song.tracks[i].id = nn;
+      player.loader.startLoad(audioContext, info.url, info.variable);
     }
     for (let i = 0; i < song.beats.length; i++) {
-      player.cacheDrum(song.beats[i].n);
+      let nn = player.loader.findDrum(song.beats[i].n);
+      let info = player.loader.drumInfo(nn);
+      song.beats[i].info = info;
+      song.beats[i].id = nn;
+      player.loader.startLoad(audioContext, info.url, info.variable);
     }
+    player.loader.waitLoad(function () {
+      console.log("Finished loading instruments");
+    });
 
     fileSelectRef.current.value = null; // clear the file so every upload is new
-  }, [currentMidi, midiSoundRef, fileSelectRef]);
+  }, [currentMidi, fileSelectRef, player, audioContext]);
 
   const play = () => {
     if (currentMidi === null) {
-      midiSoundRef.current.playChordNow(3, [60], 3);
+      console.log("not ready");
       return;
     }
-    const player = midiSoundRef.current;
     const song = currentMidi;
-    const songStart = player.contextTime();
+    const songStart = audioContext.currentTime;
     const stepDuration = 44 / 1000; // 44ms notes to play;
 
-    let currentTime = player.contextTime();
+    let currentTime = songStart;
     let currentSongTime = 0;
-    let nextStepTime = currentTime;
+    let nextStepTime = songStart;
     while (currentTime < songStart + song.duration) {
       if (currentTime > nextStepTime - stepDuration) {
         sendNotes(
@@ -62,7 +77,7 @@ const App = () => {
         currentSongTime += stepDuration;
         nextStepTime += stepDuration;
       }
-      currentTime = player.contextTime();
+      currentTime = audioContext.currentTime;
     }
   };
 
@@ -79,8 +94,19 @@ const App = () => {
           if (duration > 3) {
             duration = 3;
           }
-          let instr = track.program;
-          player.playChordAt(when, instr, [track.notes[i].pitch], duration);
+          let instr = track.info.variable;
+          let v = track.volume / 7;
+
+          player.queueWaveTable(
+            audioContext,
+            input,
+            window[instr], // window contains all the zone information for the instruments, seems to be configs to make it sound better, it is populated during loading
+            when,
+            track.notes[i].pitch,
+            duration,
+            v,
+            track.notes[i].slides
+          );
         }
       }
     }
@@ -90,9 +116,18 @@ const App = () => {
       for (let i = 0; i < beat.notes.length; i++) {
         if (beat.notes[i].when >= start && beat.notes[i].when < end) {
           let when = songStart + beat.notes[i].when;
-          let instr = beat.n;
-          console.log({ when, instr });
-          player.playDrumsAt(when, [instr]);
+          let duration = 1.5;
+          let instr = beat.info.variable;
+          let v = beat.volume / 2;
+          player.queueWaveTable(
+            audioContext,
+            input,
+            window[instr],
+            when,
+            beat.n,
+            duration,
+            v
+          );
         }
       }
     }
@@ -111,11 +146,6 @@ const App = () => {
           ref={fileSelectRef}
         />
         <button onClick={() => play()}>Play</button>
-        <MIDISounds
-          ref={midiSoundRef}
-          appElementName="root"
-          instruments={[3]}
-        />
       </header>
     </div>
   );
